5c2dbda60022b743130c44b0304ca00dbadeaedd
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2020-08-03 07:52:23 +0900

    add std::set<std::uint32_t> comparing operator== to test.cpp

--- network_test/test.cpp
+++ network_test/test.cpp
@@ -6,9 +6,24 @@
 #include <gmock/gmock.h>
 #include "graph_algorithm.h"
 
+bool operator==(const std::set<std::uint32_t>& s1, const std::set<std::uint32_t>& s2) {
+    auto iter1 = s1.begin(), iter2 = s2.begin();
+    for(; iter1 != s1.end() && iter2 != s2.end(); iter1++, iter2++) {
+        if(*iter1 != *iter2) return false;
+    }
+    return iter1 == s1.end() && iter2 == s2.end();
+}
+
 namespace {
+    TEST(ALGORITHM_TEST, OPERATOR_EQUAL) {
+        EXPECT_TRUE((std::set<std::uint32_t> {1, 2, 3, 4} == std::set<std::uint32_t> {1, 2, 3, 4}));
+        EXPECT_TRUE((std::set<std::uint32_t> {1, 2, 4, 3} == std::set<std::uint32_t> {1, 2, 3, 4}));
+        EXPECT_TRUE(!(std::set<std::uint32_t> {1, 3, 3, 4} == std::set<std::uint32_t> {1, 2, 3, 4}));
+        EXPECT_TRUE(!(std::set<std::uint32_t> {1, 5, 3, 4} == std::set<std::uint32_t> {1, 2, 3, 4}));
+    }
+
     TEST(ALGORITHM_TEST, FIND_SOURCE) {
-        
+
     }
 }
 
