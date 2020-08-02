8c0c6da2a28d4ffac995e77e4752031fa1e1bcdd
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2020-08-03 07:31:23 +0900

    add test framework

---
+++ src/graph_algorithm.cpp
@@ -0,0 +1,30 @@
+//
+// Created by hungr on 2020/07/30.
+//
+#include "graph_algorithm.h"
+#include <algorithm>
+#include <list>
+
+
+bool is_acyclic(std::uint32_t node_count, const std::vector<std::pair<std::uint32_t, std::uint32_t>>& connection_rule) {
+    std::list<std::uint32_t> sort_result;
+    std::set<std::uint32_t> source;
+
+    while(!source.empty()) {
+
+    }
+}
+
+void find_source(std::uint32_t node_count,
+                 const std::vector<std::pair<std::uint32_t, std::uint32_t>>& connection_rule,
+                 std::set<std::uint32_t>& source) {
+    for(auto n = 0; n < node_count; n++) {
+        if(std::find_if(
+                connection_rule.begin(),
+                connection_rule.end(),
+                [n](auto&& c) { return c.second == n; })
+           == connection_rule.end()) {
+            source.insert(n);
+        }
+    }
+}
